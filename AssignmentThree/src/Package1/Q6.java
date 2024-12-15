625d89f980f3f2050e10a7b4dd9cf9cad1b54d3a
Tanmay Nayak <tanmaya0301@gmail.com>
2024-12-15 20:52:50 +0530

    Update Q6.java

--- AssignmentThree/src/Package1/Q6.java
+++ AssignmentThree/src/Package1/Q6.java
@@ -1,29 +1,23 @@
 package Package1;
 import java.util.Scanner;
 public class Q6 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-	public static void main(String[] args) {
-	Scanner input = new Scanner(System.in);
-	System.out.print("Enter the Units: ");
-	double unt = input.nextDouble();
-	double bill = 0;
-	if (unt<= 50) {
-		bill = unt*3;
-	}
-	else if (unt>=50 && unt<=200) {
-		bill = (50*3) + ((unt-50) * 4.80);
-	}
-	else if (unt>=200 && unt<=400) {
-		bill = (50*3) + (150 * 4.80) + ((unt-200) *5.8);
-	}
-	else {
-		bill = (50*3) + (150 * 4.80) + (200 *480) + ((unt-400) * 6.20);
-	}
-	
-	System.out.println("Electricity Bill is "+ bill);
-	System.out.printf("Electricity Bill is %.2f\n", bill); // for up to 2 decimal place
-	System.out.printf("Electricity Bill is %.4f", bill); // must use \n for new line
-	
-	}
+        System.out.print("Enter the Units: ");
+        double unt = input.nextDouble();
+        double bill = 0;
 
+        if (unt <= 50) {
+            bill = unt * 3;
+        } else if (unt > 50 && unt <= 200) {
+            bill = (50 * 3) + ((unt - 50) * 4.80);
+        } else if (unt > 200 && unt <= 400) {
+            bill = (50 * 3) + (150 * 4.80) + ((unt - 200) * 5.80);
+        } else {
+            bill = (50 * 3) + (150 * 4.80) + (200 * 5.80) + ((unt - 400) * 6.20);
+        }
+
+        System.out.printf("Electricity Bill is: Rs. %.2f\n", bill); // Up to 2 decimal places
+    }
 }
