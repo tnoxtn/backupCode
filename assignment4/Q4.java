0634e2cbe60e90ac38ec6dee90e804649f546482
Tanmay Nayak <tanmaya0301@gmail.com>
2025-01-06 21:56:38 +0530

    Update Q4.java

--- assignment4/Q4.java
+++ assignment4/Q4.java
@@ -1,31 +1,58 @@
 package assignment4;
 import java.util.Scanner;
+
 public class Q4 {
 
-	public static void main(String[] args) {
-		Scanner in = new Scanner (System.in);
-		System.out.print("Enter 1st Number: ");
-		int x = in.nextInt();
-		System.out.print("Enter 2nd Number: ");
-		int y = in.nextInt();
-		int temp = 0;
-		int ans = x % y;
-		if (x==y) {
-			System.out.println("GCD of x and y is" + y);
-		}
-		else {
-			while(y!=0) {
-				temp = y;
-				y = x % y;
-				x = temp;
-			}
-		}
-		System.out.println("GCD of x and y is " + x);
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        // Get input from user
+        System.out.print("Enter the first number: ");
+        int x = inp.nextInt();
+        System.out.print("Enter the second number: ");
+        int y = inp.nextInt();
+
+        // Implementing Euclid's algorithm in steps
+        while (x % y != 0) {  // Check if y divides x
+            int remainder = x % y;  // Compute the remainder
+            x = y;                  // Update x
+            y = remainder;          // Update y
+        }
+
+        // When the loop exits, y is the GCD
+        System.out.println("GCD of the given numbers is " + y);
+    }
+}
+
+
+
+// import java.util.Scanner;
+// public class Q4 {
+
+// 	public static void main(String[] args) {
+// 		Scanner in = new Scanner (System.in);
+// 		System.out.print("Enter 1st Number: ");
+// 		int x = in.nextInt();
+// 		System.out.print("Enter 2nd Number: ");
+// 		int y = in.nextInt();
+// 		int temp = 0;
+// 		int ans = x % y;
+// 		if (x==y) {
+// 			System.out.println("GCD of x and y is" + y);
+// 		}
+// 		else {
+// 			while(y!=0) {
+// 				temp = y;
+// 				y = x % y;
+// 				x = temp;
+// 			}
+// 		}
+// 		System.out.println("GCD of x and y is " + x);
 		
 
-	}
+// 	}
 
-}
+// }
 
 //output
 //Enter 1st Number: 98
