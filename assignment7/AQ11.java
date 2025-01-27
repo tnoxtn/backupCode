3aa8de4f37e70801cd0ddf9b90240f438974d2b0
Tanmay Nayak <tanmaya0301@gmail.com>
2025-01-27 21:55:03 +0530

    Update and rename q11.java to AQ11.java

--- assignment7/AQ11.java
+++ assignment7/AQ11.java
@@ -1,7 +1,7 @@
 
 package assignment7;
 import java.util.Scanner;
-public class q11 {
+public class AQ11 {
 	public static void main(String[] args) {
 	Scanner in = new Scanner (System.in);
 	System.out.print("Enter 10 Elements: ");
