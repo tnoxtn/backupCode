98b4fbc509595dc6a8ce0ec3ed72c9984a40c1aa
Tanmay Nayak <tanmaya0301@gmail.com>
2025-01-27 21:43:58 +0530

    Update Q3.java

--- assignment7/Q3.java
+++ assignment7/Q3.java
@@ -1,30 +1,36 @@
-package assignment7;
+package array;
 import java.util.Scanner;
-public class Q3 {
-	public static void main(String[] args) {
-	Scanner in = new Scanner (System.in);
-	System.out.print("Enter 10 Elements: ");
-	int arr[] = new int [10];
-	for(int x=0; x<arr.length; x++) {
-		arr[x] = in.nextInt();
-	}
-	System.out.println();
-	System.out.print("enter the number to be searched: ");
-	int search = in.nextInt();
-	
-	int count = 0;
-	
-	for(int num: arr) {
-		if(num == search) {
-			count++;
+public class AQ03 {
+
+		public static void main(String[] args) {
+		Scanner in = new Scanner (System.in);
+		System.out.print("Enter 10 Elements: ");
+		int arr[] = new int [10];
+		for(int x=0; x<arr.length; x++) {
+			arr[x] = in.nextInt();
 		}
+		
+		System.out.println("Enter the number to be searched: ");
+		int search = in.nextInt();
+		
+		int count = 0;
+		
+		for(int num: arr) {
+			if(num == search) {
+				count++;
+			}
+		}
+		
+		System.out.println(search + " is present "+ count + " times");
+
+
 	}
+	}
+//Enter 10 Elements: 5 4 5 2 6 1 5 2 4 6
+//Enter the number to be searched: 
+//5
+//5 is present 3 times
 	
-	System.out.println(search + " is present "+ count + " times");
-
-
-}
-}
-//Enter 10 Elements: 9 8 7 6 5 5 4 3 2 1
-//enter the number to be searched: 5
-//5 is present 2 times
+	
+	
+	
