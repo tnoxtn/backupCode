3c08bf57dc1a4be8ace2c0213433bca5ca1e5beb
Tanmay Nayak <tanmaya0301@gmail.com>
2025-06-02 08:40:26 +0530

    Add files via upload

--- q1.java
+++ q1.java
@@ -1,72 +1,77 @@
-package assignment5;
+package circular;
 import java.util.*;
 
-public class q1 {
-	static int top = -1;
-	static final int max = 10;
+class Node{
+	int data;
+	Node next;
 	
-	public static boolean isEmpty() {
-		return top==-1;
-	}
-	public static boolean isFull() {
-		return top==max-1;
-	}
-	
-	public static int push(int s[]) {
-		if(isFull()) {
-			System.out.println("Stack Overflow");
-			return top;
+	Node(){
+		Scanner in = new Scanner(System.in);
+		data = in.nextInt();
 		}
-		Scanner sc = new Scanner (System.in);
-		System.out.println("Enter Element: ");
-		int item = sc.nextInt();
-		top++;
-		s[top]=item;
-		
-		return top;
-	}
-	
-	public static int pop(int s[]) {
-		if(isEmpty()) {
-			System.out.println("Satck Underflow");
-			return top;
-		}
-		System.out.println("Deleted Element: "+ s[top]);
-		top--;
-		
-		return top;
-	}
-	
-	public static void display(int s[]) {
-		if(isEmpty()) {
-			System.out.println("Satck Underflow");
-			return;
-		}
-		for(int i = top; i>=0; i--) {
-			System.out.println(s[i]);
-		}
-	}
-	
+}
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner (System.in);
-		int arr[] = new int[max];
+public class q1 {
+	
+	public static Node create(Node start) {
+		Node newnode = new Node();
+		start = newnode;
+		start.next= start;
 		
+		Node temp = start;
+		Scanner in = new Scanner(System.in);
 		while(true) {
-		System.out.println("Enter the operation: ");
-		int op = sc.nextInt();
+		System.out.println("Do you want to add more node (y/n)");
+		char c = in.next().charAt(0);
 		
-		switch(op) {
-		case 0: System.exit(0);
-		case 1: isEmpty(); break;
-		case 2: isFull(); break;
-		case 3: top = push(arr); break;
-		case 4: top= pop(arr); break;
-		case 5: display(arr); break;
-		default: System.out.println("invalid");
-		}
+		if(c=='n'||c=='N') {
+			break;
 		}
 		
+		Node newernode = new Node();
+		newernode.next = start;
+		temp.next = newernode;
+		temp = temp.next;
+	}
+		return start;
+	}
+	
+	public static void display(Node start) {
+		Node temp = start;
+		do {
+			System.out.println("DATA: "+temp.data);
+			temp=temp.next;
+		}while(temp!=start);
+	}
+	
+	public static Node insBeg (Node start) {
+		Node newNode = new Node();
+
+		Node temp = start;
+//		do {
+//			temp=temp.next;
+//		}while(temp!=start);
+		while(temp.next!=start) {
+			temp=temp.next;
+		}
+		
+		newNode.next = start;
+		temp.next = newNode;
+		start = newNode;
+		
+		return start;
+	}
+	
+	public static void main (String[] args) {
+		Node start = null;
+		
+		start = create(start);
+		
+		display(start);
+		
+		start = insBeg(start);
+		
+		display(start);
 	}
 
 }
